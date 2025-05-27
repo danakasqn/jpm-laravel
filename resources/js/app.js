8675783dc003582ad9aa4626b8fa3bd506eb6e1f
Patryk <jd.patrykk@gmail.com>
2025-05-27 21:14:07 +0200

    Force Bootstrap collapse activation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
-import 'bootstrap';
+import 'bootstrap'; // ← to jest kluczowe!
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+import { Collapse } from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const collapseElements = document.querySelectorAll('.collapse');
+    collapseElements.forEach(el => new Collapse(el));
+});

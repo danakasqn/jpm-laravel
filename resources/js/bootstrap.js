b8e28bc38c7b4f6f0503685ca41cd26420d38251
Patryk <jd.patrykk@gmail.com>
2025-05-27 21:17:46 +0200

    Fix: dodanie Bootstrap JS dla poprawnego działania collapse

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Bootstrap JS (w tym Collapse, Dropdown, Modal itd.)
+import 'bootstrap';
